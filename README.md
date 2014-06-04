6918b0e6a4b0e1d81011b8dd739bdbd7fd120922
Mate Lakat <mlakat@mlakat.hu>
2014-06-04 20:28:36 +0100

    Install and start tomcat

--- README.md
+++ README.md
@@ -4,5 +4,9 @@ Get out some coverage data for a webapp
 
 ## Requirements
 
+You use a Linux machine.
+
 Download the JDK from oracle. Put the download targz to the directory
 `server-jre`
+
+## Start TomCat
---
+++ install_start_tomcat.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -eux
+
+(
+cd tomcat-dl/
+wget -nc http://mirror.gopotato.co.uk/apache/tomcat/tomcat-6/v6.0.41/bin/apache-tomcat-6.0.41.tar.gz
+)
+
+TOMCAT_INSTALL=$(find tomcat-dl -name "*.tar.gz")
+TOMCAT_INSTALL=$(readlink -f $TOMCAT_INSTALL)
+
+SERVER_JRE_INSTALL=$(find server-jre/ -name "*.tar.gz")
+
+[ -n "$SERVER_JRE_INSTALL" ]
+
+SERVER_JRE_INSTALL=$(readlink -f $SERVER_JRE_INSTALL)
+INSTALLED_SERVER_JRE=$(find installed-server-jre -mindepth 1 -maxdepth 1 -type d)
+
+if [ -z "$INSTALLED_SERVER_JRE" ]; then
+(
+    cd installed-server-jre/
+    EXTRACTED_JRE=$(find -mindepth 1 -maxdepth 1 -type d)
+    if [ -z "$EXTRACTED_JRE" ] ; then
+        tar -xzf $SERVER_JRE_INSTALL
+    fi
+)
+fi
+
+INSTALLED_TOMCAT=$(find installed-tomcat -mindepth 1 -maxdepth 1 -type d)
+
+if [ -z "$INSTALLED_TOMCAT" ]; then
+(
+    cd installed-tomcat/
+    tar -xzf "$TOMCAT_INSTALL"
+)
+fi
+
+INSTALLED_TOMCAT=$(find installed-tomcat -mindepth 1 -maxdepth 1 -type d)
+INSTALLED_TOMCAT=$(readlink -f $INSTALLED_TOMCAT)
+
+JAVA_HOME=$INSTALLED_SERVER_JRE $INSTALLED_TOMCAT/bin/catalina.sh start
